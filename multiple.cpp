7189d3d26a3edc616e0e2f8c567edd07630c2649
Balew83 <balewmolla709@gmail.com>
2025-04-08 02:19:34 +0300

    Update multiple.cpp

--- multiple.cpp
+++ multiple.cpp
@@ -4,18 +4,14 @@ using namespace std;
 
 int main() {
     double num1, num2, result;
-
-    // Ask for input
+    
     cout << "Enter first number: ";
     cin >> num1;
 
     cout << "Enter second number: ";
     cin >> num2;
-
-    // Multiply the numbers
     result = num1 * num2;
 
-    // Display the result
     cout << "The result of multiplication is: " << result << endl;
 
     return 0;
